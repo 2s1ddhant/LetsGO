2c4fc178957e072c2d41518554ef54fa6ea1e300
2s1ddhant <35763967+2s1ddhant@users.noreply.github.com>
2021-09-17 12:50:18 +0530

    Master (#1)
    * some integer practice
    
    * int and const prac
    
    Co-authored-by: Siddhant More <simore@simore.pnq.csb>

---
+++ Ints/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Sid")
+	fmt.Println(age, name, " more")
+	fmt.Println("")
+	fmt.Println("Hi my name is", name, "and my roll number is", age)
+}
+
+var age int = 7
+var name string = "siddhant"
+
+// fmt.Println(age , name)
